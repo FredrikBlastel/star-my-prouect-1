2623baaacc39f6e170985dd307598f16b3def741
FredrikBlastel <116519673+FredrikBlastel@users.noreply.github.com>
2022-10-23 16:11:03 -0500

    this is my first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+alert('bienvenidos a mi pagina de filosofia');
+
+let nombre = "AH0RA SI MANOS A LA OBRA";
+let altura = 165; 
+
+let concatenacion = nombre + " " + altura;
+
+document.write(concatenacion);
